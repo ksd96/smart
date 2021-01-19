16ccb6a3092dd6b53327dbd373725add6cc43162
ksd96 <45177616+ksd96@users.noreply.github.com>
2021-01-19 16:56:46 +0300

    добавляет подвал сайта

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,30 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+const footerButtons = document.querySelectorAll(`.footer__button`);
+const footerLists = document.querySelectorAll(`.footer__item`);
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+function classToggle(element, firstClass, lastClass) {
+  element.classList.toggle(`${firstClass}`);
+  element.classList.toggle(`${lastClass}`);
+}
+
+footerButtons.forEach((button) => {
+  button.addEventListener(`click`, (evt) => {
+    if(button.classList.contains(`footer__button_closed`)) {
+      footerButtons.forEach((item) => {
+        if(item.classList.contains(`footer__button_opened`)) {
+          classToggle(item, `footer__button_closed`, `footer__button_opened`);
+        }
+      });
+      footerLists.forEach((list) => {
+        if(list.classList.contains(`footer__item_opened`)) {
+          classToggle(list, `footer__item_closed`, `footer__item_opened`);
+        }
+      });
+    };
+    classToggle(button, `footer__button_closed`, `footer__button_opened`);
+
+    const list = evt.target.parentElement.parentElement.querySelector(`.footer__item`);
+    classToggle(list, `footer__item_closed`, `footer__item_opened`);
+  });
 });
